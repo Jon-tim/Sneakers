46d39f0bc4bf3b392efc5efe4d5df1c1ffa940fa
jontim <awoniyitimi@outlook.com>
2022-10-28 19:46:39 +0100

    fix

--- src/features/Main/MainContent.jsx
+++ src/features/Main/MainContent.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import "../Styles/main.scss";
 import DesktopSwiper from "../../component/DesktopSwiper";
-// import MobileSwiper from "../../../src/component/MobileSwiper";
 import MobileSwiper from "../../component/MobileSwiper";
 import { useState } from "react";
 
