0ed3ae0fcc9bc111fe8e00af3df71a57618a5871
jontim <awoniyitimi@outlook.com>
2022-10-29 23:27:27 +0100

    fix: Fix image display

--- src/component/MobileSwiper.jsx
+++ src/component/MobileSwiper.jsx
@@ -24,25 +24,22 @@ export default function MobileSwiper() {
       >
         <SwiperSlide>
           <img
-            src="../src/assets/images/image-product-1.jpg"
+            src="../../src/assets/images/image-product-1.jpg"
+            alt="image product"
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="../../src/assets/images/image-product-2.jpg" alt="image product" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            src="../../src/assets/images/image-product-3.jpg"
             alt="image product"
           />
         </SwiperSlide>
         <SwiperSlide>
           <img
-            src="src/assets/images/image-product-2.jpg"
-            alt="image product"
-          />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img
-            src="../src/assets/images/image-product-3.jpg"
-            alt="image product"
-          />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img
-            src="../src/assets/images/image-product-4.jpg"
+            src="../../src/assets/images/image-product-4.jpg"
             alt="image product"
           />
         </SwiperSlide>
